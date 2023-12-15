ea8940b142a83a2a3bc4d781e97d5a3cdded7522
Yang Chen <60239063+yangchen2@users.noreply.github.com>
2023-12-15 13:54:57 -0800

    filtered tables and metadata

--- workflow/rules/pipeline.smk
+++ workflow/rules/pipeline.smk
@@ -1,4 +1,5 @@
 import os
+import biom
 
 # Define your partitions
 partitions = ["skin_and_gut_disease_pos", "skin_pos_and_gut_disease_neg", "skin_neg_and_gut_disease_pos", "skin_and_gut_disease_neg"]
@@ -9,14 +10,14 @@ rule filter_duplicate_samples:
         biom_table="../data_download/tables/{partition}.biom",
         metadata="../data_download/metadata/{partition}_metadata.tsv"
     output:
-        "output/filtered_tables/{partition}_filtered.biom",
-        "output/filtered_metadata/{partition}_filtered.tsv"
+        biom_table_filtered="output/filtered_tables/{partition}_filtered.biom",
+        metadata_filtered="output/filtered_metadata/{partition}_filtered.tsv"
     params:
         script="scripts/filter_duplicate_samples.py"
-    log: 
-        "log/{partition}/filter_duplicate_samples.log"
+    # log: 
+    #     "workflow/log/{partition}/filter_duplicate_samples.log"
     shell:
-        "python {params.script} {input.biom_table} {input.metadata} {output} {wildcards.partition}"
+        "python {params.script} {input.biom_table} {input.metadata} {output.biom_table_filtered} {output.metadata_filtered} {wildcards.partition}"
 
 all_tables = expand(
     "output/filtered_tables/{partition}_filtered.biom",
--- workflow/scripts/filter_duplicate_samples.py
+++ workflow/scripts/filter_duplicate_samples.py
@@ -2,34 +2,43 @@ import biom
 from biom.util import biom_open
 from biom import Table
 import pandas as pd
+import numpy as np
+import csv
+from pathlib import Path
 import logging
 import sys
 
 # Set up logging
-logging.basicConfig(filename='../log/{partition}/filter_duplicate_samples.log', level=logging.INFO)
+# logging.basicConfig(filename="workflow/log/{partition}/filter_duplicate_samples.log", level=logging.INFO)
 
 def filter_datasets(biom_table, metadata, biom_output_file, metadata_output_file, partition):
-    # drop duplicate rows in the metadata df
-    logging.info(f'Starting number of samples: {metadata.shape[0]}')
+    # Load biom table
+    with biom_open(biom_table) as f:
+        biom_table = Table.from_hdf5(f)
+
+    # Load metadata 
+    metadata = pd.read_csv(metadata, sep='\t').set_index('#SampleID')
+
+    # Drop duplicate samples from same individuals in the metadata df
+    # logging.info(f'Starting number of samples: {metadata.shape[0]}')
     metadata_filtered = metadata.drop_duplicates(subset='host_subject_id', keep='first')
-    logging.info(f'Number of samples after dropping duplicates: {metadata_filtered.shape[0]}')
+    # logging.info(f'Number of samples after dropping duplicates: {metadata_filtered.shape[0]}')
 
     # Save filtered metadata
     metadata_filtered.to_csv(metadata_output_file, sep='\t', index=False)
 
-    # Load biom table
-    with biom_open(biom_table) as f:
-        table = Table.from_hdf5(f)
+    # Subset the biom table based on the filtered metadata
+    biom_df = biom_table.to_dataframe().transpose()
+    biom_df_filtered = biom_df.loc[metadata_filtered.index]
 
-    # Identify duplicate samples
-    duplicate_samples = metadata[metadata.duplicated('host_subject_id')]['host_subject_id'].tolist()
-
-    # Remove duplicate samples from biom table
-    table.filter(duplicate_samples, axis='sample', inplace=True)
-
-    # Save filtered biom table
+    # Convert the DataFrame to a BIOM table
+    obs_ids = biom_df_filtered.index
+    samp_ids = biom_df_filtered.columns
+    table = biom.table.Table(biom_df_filtered.values, observation_ids=obs_ids, sample_ids=samp_ids)
+    
+    # Save the BIOM table
     with biom_open(biom_output_file, 'w') as f:
-        table.to_hdf5(f)
+        table.to_hdf5(f, generated_by="filtered table")
 
 if __name__ == "__main__":
     if len(sys.argv) != 6:
